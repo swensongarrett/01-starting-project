aea3ee2971148165fd72cfcee87eca9843d081bb
Garrett <swensongarrett@gmail.com>
2026-02-03 18:15:46 -0700

    delete tasks when complete is pressed

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -42,4 +42,8 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
 }
